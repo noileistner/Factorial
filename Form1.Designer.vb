8d0a878b930a03180b0de002af53badf82dd4104
noile <noile@NOIS-MATEBOOK>
2022-12-15 15:57:46 +0100

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,72 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.LabelResult = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'TextBox
+        '
+        Me.TextBox.Location = New System.Drawing.Point(163, 28)
+        Me.TextBox.Name = "TextBox"
+        Me.TextBox.Size = New System.Drawing.Size(181, 22)
+        Me.TextBox.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(163, 65)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(179, 42)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'LabelResult
+        '
+        Me.LabelResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.LabelResult.Location = New System.Drawing.Point(163, 125)
+        Me.LabelResult.Name = "LabelResult"
+        Me.LabelResult.Size = New System.Drawing.Size(181, 88)
+        Me.LabelResult.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.LabelResult)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox)
+        Me.Name = "Form1"
+        Me.Text = "Factorial (NLS)"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents LabelResult As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+    Dim Number As Integer
+    Dim x As Integer
+    Dim f As Long
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Number = TextBox.Text
+
+        If Number > 20 Then
+            LabelResult.Text = "No puc calcular el factorial de números majors que 20!"
+            Return
+        End If
+
+        f = 1
+        For x = 1 To Number
+            f *= x
+        Next
+        LabelResult.Text = Str(f)
+    End Sub
+End Class
